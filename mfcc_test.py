b6d5c916c6ba29f0c737f136115cecaa31d35370
Steven <goingtogaming@gmail.com>
2018-09-20 13:24:06 +0200

    Added mfcc working plot file

---
+++ mfcc_test.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+import librosa
+import librosa.display
+from scipy.io import wavfile as wav
+
+audiofile = 'AudioFiles/KamerMatthijs1.wav'
+
+x, fs = librosa.load(audiofile)
+mfccs = librosa.feature.mfcc(x, sr=fs)
+
+plt.title(audiofile)
+plt.plot(mfccs)
+plt.show()
+
+'''
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(mfccs, x_axis='time')
+plt.colorbar()
+plt.tight_layout()
+plt.show()
+'''
