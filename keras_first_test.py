28ea606fb69c23add3e210849d6a72a95109e3ef
StevenJW <goingtogaming@gmail.com>
2018-10-23 15:34:36 +0200

    Changed keras to new API

--- keras_first_test.py
+++ keras_first_test.py
@@ -5,13 +5,13 @@ from data_conversion import convert_train_data
 x_train, y_train = convert_train_data('E')
 
 model = Sequential()
-model.add(Dense(800, input_dim=800, init='uniform'))
+model.add(Dense(800, kernel_initializer="uniform", input_dim=800))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(800, init='uniform'))
+model.add(Dense(800, kernel_initializer="uniform"))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(2, init='uniform'))
+model.add(Dense(2, kernel_initializer="uniform"))
 model.add(Activation('softmax'))
 
 model.compile(loss='mean_squared_error', optimizer='adam')
